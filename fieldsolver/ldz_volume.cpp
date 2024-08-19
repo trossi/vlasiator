a77815ad043ff657c26d5a9178cdf04b978d994f
Tuomas Rossi <tuomas.rossi@csc.fi>
2024-08-19 11:33:34 +0300

    Simplify repeated code

--- fieldsolver/ldz_volume.cpp
+++ fieldsolver/ldz_volume.cpp
@@ -30,6 +30,39 @@
 #endif
 
 using namespace std;
+using index_t = FsGridTools::FsIndex_t;
+
+
+template<typename SomeFsGrid>
+inline static auto get(
+   index_t i, index_t j, index_t k,
+   SomeFsGrid & grid,
+   const TechnicalFsGrid & technicalGrid
+) {
+   #ifdef DEBUG_FSOLVER
+   if (technicalGrid.get(i,j,k) == NULL) {
+      stringstream ss;
+      ss << "ERROR, got NULL neighbor in " << __FILE__ << ":" << __LINE__ << endl;
+      cerr << ss.str(); exit(1);
+   }
+   #endif
+   return grid.get(i,j,k);
+}
+
+
+template<long unsigned int N, typename T>
+inline static auto calculateAverage(
+   const T & grid_0, const T & grid_1, const T & grid_2, const T & grid_3
+) {
+   CHECK_FLOAT(grid_0->at(N));
+   CHECK_FLOAT(grid_1->at(N));
+   CHECK_FLOAT(grid_2->at(N));
+   CHECK_FLOAT(grid_3->at(N));
+   auto val = FOURTH*(grid_0->at(N) + grid_1->at(N) + grid_2->at(N) + grid_3->at(N));
+   CHECK_FLOAT(val);
+   return val;
+}
+
 
 void calculateVolumeAveragedFields(
    BFieldFsGrid & perBGrid,
@@ -38,7 +71,6 @@ void calculateVolumeAveragedFields(
    VolFsGrid & volGrid,
    TechnicalFsGrid & technicalGrid
 ) {
-   //const auto gridDims = technicalGrid.getLocalSize();
    const auto gridDims = &technicalGrid.getLocalSize()[0];
    const size_t N_cells = gridDims[0]*gridDims[1]*gridDims[2];
 
@@ -48,9 +80,9 @@ void calculateVolumeAveragedFields(
    {
       phiprof::Timer parallelTimer {parallelTimerId};
       #pragma omp for collapse(2)
-      for (FsGridTools::FsIndex_t k=0; k<gridDims[2]; k++) {
-         for (FsGridTools::FsIndex_t j=0; j<gridDims[1]; j++) {
-            for (FsGridTools::FsIndex_t i=0; i<gridDims[0]; i++) {
+      for (index_t k=0; k<gridDims[2]; k++) {
+         for (index_t j=0; j<gridDims[1]; j++) {
+            for (index_t i=0; i<gridDims[0]; i++) {
                std::array<Real, Rec::N_REC_COEFFICIENTS> perturbedCoefficients;
                auto volGrid0 = volGrid.get(i,j,k);
 
@@ -73,92 +105,34 @@ void calculateVolumeAveragedFields(
 
                // This avoids out of domain accesses below.
                if(technicalGrid.get(i,j,k)->sysBoundaryFlag == sysboundarytype::DO_NOT_COMPUTE || technicalGrid.get(i,j,k)->sysBoundaryFlag == sysboundarytype::OUTER_BOUNDARY_PADDING) continue;
-               // Calculate volume average of E (FIXME NEEDS IMPROVEMENT):
-               auto EGrid_i1j1k1 = EGrid.get(i,j,k);
+
+               // Calculate volume average of E
                if ( technicalGrid.get(i,j,k)->sysBoundaryFlag == sysboundarytype::NOT_SYSBOUNDARY ||
                     (technicalGrid.get(i,j,k)->sysBoundaryFlag != sysboundarytype::NOT_SYSBOUNDARY && technicalGrid.get(i,j,k)->sysBoundaryLayer == 1)
                   ) {
-                  #ifdef DEBUG_FSOLVER
-                  bool ok = true;
-                  if (technicalGrid.get(i  ,j+1,k  ) == NULL) ok = false;
-                  if (technicalGrid.get(i  ,j  ,k+1) == NULL) ok = false;
-                  if (technicalGrid.get(i  ,j+1,k+1) == NULL) ok = false;
-                  if (ok == false) {
-                     stringstream ss;
-                     ss << "ERROR, got NULL neighbor in " << __FILE__ << ":" << __LINE__ << endl;
-                     cerr << ss.str(); exit(1);
-                  }
-                  #endif
+                  auto EGrid_i1j1k1 = get(i  ,j  ,k  , EGrid, technicalGrid);
+                  auto EGrid_i1j1k2 = get(i  ,j  ,k+1, EGrid, technicalGrid);
+                  auto EGrid_i1j2k1 = get(i  ,j+1,k  , EGrid, technicalGrid);
+                  auto EGrid_i1j2k2 = get(i  ,j+1,k+1, EGrid, technicalGrid);
+                  auto EGrid_i2j1k1 = get(i+1,j  ,k  , EGrid, technicalGrid);
+                  auto EGrid_i2j1k2 = get(i+1,j  ,k+1, EGrid, technicalGrid);
+                  auto EGrid_i2j2k1 = get(i+1,j+1,k  , EGrid, technicalGrid);
 
-                  auto EGrid_i1j2k1 = EGrid.get(i  ,j+1,k  );
-                  auto EGrid_i1j1k2 = EGrid.get(i  ,j  ,k+1);
-                  auto EGrid_i1j2k2 = EGrid.get(i  ,j+1,k+1);
-
-                  CHECK_FLOAT(EGrid_i1j1k1->at(fsgrids::efield::EX));
-                  CHECK_FLOAT(EGrid_i1j2k1->at(fsgrids::efield::EX));
-                  CHECK_FLOAT(EGrid_i1j1k2->at(fsgrids::efield::EX));
-                  CHECK_FLOAT(EGrid_i1j2k2->at(fsgrids::efield::EX));
-                  volGrid0->at(fsgrids::volfields::EXVOL) = FOURTH*(EGrid_i1j1k1->at(fsgrids::efield::EX) + EGrid_i1j2k1->at(fsgrids::efield::EX) + EGrid_i1j1k2->at(fsgrids::efield::EX) + EGrid_i1j2k2->at(fsgrids::efield::EX));
-                  CHECK_FLOAT(volGrid0->at(fsgrids::volfields::EXVOL));
+                  volGrid0->at(fsgrids::volfields::EXVOL) =
+                     calculateAverage<fsgrids::efield::EX>(
+                        EGrid_i1j1k1, EGrid_i1j2k1, EGrid_i1j1k2, EGrid_i1j2k2
+                     );
+                  volGrid0->at(fsgrids::volfields::EYVOL) =
+                     calculateAverage<fsgrids::efield::EY>(
+                        EGrid_i1j1k1, EGrid_i2j1k1, EGrid_i1j1k2, EGrid_i2j1k2
+                     );
+                  volGrid0->at(fsgrids::volfields::EZVOL) =
+                     calculateAverage<fsgrids::efield::EZ>(
+                        EGrid_i1j1k1, EGrid_i2j1k1, EGrid_i1j2k1, EGrid_i2j2k1
+                     );
                } else {
                   volGrid0->at(fsgrids::volfields::EXVOL) = 0.0;
-               }
-
-               if ( technicalGrid.get(i,j,k)->sysBoundaryFlag == sysboundarytype::NOT_SYSBOUNDARY ||
-                    (technicalGrid.get(i,j,k)->sysBoundaryFlag != sysboundarytype::NOT_SYSBOUNDARY && technicalGrid.get(i,j,k)->sysBoundaryLayer == 1)
-                  ) {
-                  #ifdef DEBUG_FSOLVER
-                  bool ok = true;
-                  if (technicalGrid.get(i+1,j  ,k  ) == NULL) ok = false;
-                  if (technicalGrid.get(i  ,j  ,k+1) == NULL) ok = false;
-                  if (technicalGrid.get(i+1,j  ,k+1) == NULL) ok = false;
-                  if (ok == false) {
-                     stringstream ss;
-                     ss << "ERROR, got NULL neighbor in " << __FILE__ << ":" << __LINE__ << endl;
-                     cerr << ss.str(); exit(1);
-                  }
-                  #endif
-
-                  auto EGrid_i2j1k1 = EGrid.get(i+1,j  ,k  );
-                  auto EGrid_i1j1k2 = EGrid.get(i  ,j  ,k+1);
-                  auto EGrid_i2j1k2 = EGrid.get(i+1,j  ,k+1);
-
-                  CHECK_FLOAT(EGrid_i1j1k1->at(fsgrids::efield::EY));
-                  CHECK_FLOAT(EGrid_i2j1k1->at(fsgrids::efield::EY));
-                  CHECK_FLOAT(EGrid_i1j1k2->at(fsgrids::efield::EY));
-                  CHECK_FLOAT(EGrid_i2j1k2->at(fsgrids::efield::EY));
-                  volGrid0->at(fsgrids::volfields::EYVOL) = FOURTH*(EGrid_i1j1k1->at(fsgrids::efield::EY) + EGrid_i2j1k1->at(fsgrids::efield::EY) + EGrid_i1j1k2->at(fsgrids::efield::EY) + EGrid_i2j1k2->at(fsgrids::efield::EY));
-                  CHECK_FLOAT(volGrid0->at(fsgrids::volfields::EYVOL));
-               } else {
                   volGrid0->at(fsgrids::volfields::EYVOL) = 0.0;
-               }
-
-               if ( technicalGrid.get(i,j,k)->sysBoundaryFlag == sysboundarytype::NOT_SYSBOUNDARY ||
-                    (technicalGrid.get(i,j,k)->sysBoundaryFlag != sysboundarytype::NOT_SYSBOUNDARY && technicalGrid.get(i,j,k)->sysBoundaryLayer == 1)
-                  ) {
-                  #ifdef DEBUG_FSOLVER
-                  bool ok = true;
-                  if (technicalGrid.get(i+1,j  ,k  ) == NULL) ok = false;
-                  if (technicalGrid.get(i  ,j+1,k  ) == NULL) ok = false;
-                  if (technicalGrid.get(i+1,j+1,k  ) == NULL) ok = false;
-                  if (ok == false) {
-                     stringstream ss;
-                     ss << "ERROR, got NULL neighbor in " << __FILE__ << ":" << __LINE__ << endl;
-                     cerr << ss.str(); exit(1);
-                  }
-                  #endif
-
-                  auto EGrid_i2j1k1 = EGrid.get(i+1,j  ,k  );
-                  auto EGrid_i1j2k1 = EGrid.get(i  ,j+1,k  );
-                  auto EGrid_i2j2k1 = EGrid.get(i+1,j+1,k  );
-
-                  CHECK_FLOAT(EGrid_i1j1k1->at(fsgrids::efield::EZ));
-                  CHECK_FLOAT(EGrid_i2j1k1->at(fsgrids::efield::EZ));
-                  CHECK_FLOAT(EGrid_i1j2k1->at(fsgrids::efield::EZ));
-                  CHECK_FLOAT(EGrid_i2j2k1->at(fsgrids::efield::EZ));
-                  volGrid0->at(fsgrids::volfields::EZVOL) = FOURTH*(EGrid_i1j1k1->at(fsgrids::efield::EZ) + EGrid_i2j1k1->at(fsgrids::efield::EZ) + EGrid_i1j2k1->at(fsgrids::efield::EZ) + EGrid_i2j2k1->at(fsgrids::efield::EZ));
-                  CHECK_FLOAT(volGrid0->at(fsgrids::volfields::EZVOL));
-               } else {
                   volGrid0->at(fsgrids::volfields::EZVOL) = 0.0;
                }
             }
